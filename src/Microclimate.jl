58bf8cae365a65444e5ff5e6b9853167ef601907
Rafael Schouten <rafaelschouten@gmail.com>
2018-08-30 01:00:50 +1000

    add typed tables

--- src/Microclimate.jl
+++ src/Microclimate.jl
@@ -1,14 +1,15 @@
 module Microclimate
 
-using DataFrames
-using Unitful
+using DataFrames,
+      TypedTables, 
+      Unitful
 
+const microclimate_increments = (0.0,2.5,5.0,10.0,15.0,20.0,30.0,50.0,100.0,200.0) .* Unitful.cm
 
-const microclimate_increments = (0.0,2.5,5.0,10.0,15.0,20.0,30.0,50.0,100.0,200.0) .* u"cm"
-
-export layer_setup,
-       layer_interpolate,
-       lin_interpolate,
+export LayerInterp,
+       layer_interp,
+       lin_interp,
+       MicroclimateTable,
        MicroclimateData
 
 abstract type AbstractMicroclimateData end
@@ -39,6 +40,33 @@ struct MicroclimateData <: AbstractMicroclimateData
   DEP::Vector{Float64}
 end
 
+struct MicroclimateTable{A,B,C,D,E,F,G,H,I,J,K,L} <: AbstractMicroclimateData
+  soil::A
+  shadsoil::B
+  metout::C
+  shadmet::D
+  soilmoist::E
+  shadmoist::F
+  humid::G
+  shadhumid::H
+  soilpot::I
+  shadpot::J
+  plant::K
+  shadplant::L
+  RAINFALL::Vector{Float64}
+  dim::Int
+  ALTT::Float64
+  REFL::Float64
+  MAXSHADES::Vector{Float64}
+  longlat::Vector{Float64}
+  nyears::Int
+  timeinterval::Int
+  minshade::Float64
+  maxshade::Float64
+  DEP::Vector{Float64}
+end
+
+
 struct LayerInterp
     lower::Int
     upper::Int
@@ -46,9 +74,8 @@ struct LayerInterp
     upperfrac::Float64
 end
 
-
 " Calculate current interpolation layers and fraction from NicheMapR data"
-layer_setup(height) = begin
+LayerInterp(height) = begin
     for (i, upper) in enumerate(microclimate_increments)
         if upper > height
             lower = microclimate_increments[i - 1]
@@ -62,17 +89,23 @@ layer_setup(height) = begin
 end
 
 " Interpolate between two layers of environmental data. "
-layer_interp(i, data, pos) = begin
-    lin_interp(data[i.lower], pos) * i.lowerfrac +
-    lin_interp(data[i.upper], pos) * i.upperfrac
+layer_interp(interp, table, pos) = begin
+    table.data[interp.lower][pos] * interp.lowerfrac +
+    table.data[interp.upper][pos] * interp.upperfrac
 end
 
+# lin_interp(array::AbstractArray, pos) = begin
+#     int = floor(Int64, pos)
+#     frac::Float64 = pos - int
+#     array[int]::Float64 * (1 - frac) + array[int + 1]::Float64 * frac
+# end
+# lin_interp(array, pos::Int) = array[pos]
 " Linear interpolation "
-lin_interp(array, pos::Number) = begin
+@inline lin_interp(table, column, pos) = begin
     int = floor(Int64, pos)
     frac::Float64 = pos - int
-    array[int] * (1 - frac) + array[int + 1] * frac
+    table[column][int] * (1 - frac) + table[column][int + 1] * frac
 end
-lin_interp(array, pos::Int) = array[pos]
+@inline lin_interp(table, column, pos::Int) = table[column][pos]
 
 end # module
